d536985b232ed46edba53baf2a73dc170bd78476
Deeksha_GitFiles <sharmadeekksha@gmail.com>
2024-09-01 21:44:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hi There!</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Clicks on us are {count}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
